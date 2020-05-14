f7e3089d621c1f12ad16eff16a8fc49a607e7c52
Student User <student@workstation.lab.example.com>
2020-05-14 11:40:43 +0000

    changed made

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,6 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 <<<<<<< HEAD
 print "Finally a change is a comiing!\n";
 =======
-print "A change is a coming!\n";;
+print "A change is a coming!!!!!!\n";;
 >>>>>>> 2bc7943d4fb7a321737c0017ba3819912e5559a6
 ?>
