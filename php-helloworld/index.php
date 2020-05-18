3839e87386cbeb620b82e995e240157c996f52f3
Student User <student@workstation.lab.example.com>
2020-05-18 11:09:55 +0000

    updated

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 print "Finally a change is a comiing!\n";
-=======
-print "A change is a coming!!!!!!\n";;
->>>>>>> 2bc7943d4fb7a321737c0017ba3819912e5559a6
 ?>
