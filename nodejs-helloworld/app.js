92688180ff706c982773fe2579e58819adc34996
Jonnathan Lopez <44004508+jonnathanlopez@users.noreply.github.com>
2020-10-20 16:21:09 -0500

    add /mars endpoint

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
