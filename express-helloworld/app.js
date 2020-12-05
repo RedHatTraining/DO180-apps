14c28715534dabda2ad488cb0ebc2c0c841ace9e
Jerome Boyer <boyerje@us.ibm.com>
2020-12-04 16:52:08 -0800

    Update to version 2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! version 2\n');
 });
 
 app.get('/mars', function (req, res) {
