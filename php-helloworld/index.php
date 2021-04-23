1e4989a9708eeb968ed091d18fb2fa8b9b212eac
matthieuthibaud <matthieu.thibaud@fr.ibm.com>
2021-04-23 16:36:32 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . ". Peut-être un petit changement? Ceci est mon test dans la branche console, depuis VIM\n";
+print "Hello, World! php version is " . PHP_VERSION . ". Là, nous avons une version modifiée avec Notepad++\n";
 ?>
