74442b0a9f99c19cf8a2058052bfee12cdaba4fc
Student User <student@workstation.lab.example.com>
2021-04-23 10:02:59 -0400

    test commit

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . ". Peut-être un petit changement?\n";
+print "Hello, World! php version is " . PHP_VERSION . ". Peut-être un petit changement? Ceci est mon test dans la branche console, depuis VIM\n";
 ?>
