8547115de41f13c6d702b6af3b0fc12c8da537be
damuvijji <55542623+damuvijji@users.noreply.github.com>
2021-12-02 13:26:11 +0530

    Delete damodar directory

--- damodar/DO180/labs/manage-review/db.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `Item` (`id` BIGINT not null auto_increment primary key, `description` VARCHAR(100), `done` INT);
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (1,'Pick up newspaper', 0);
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (2,'Buy groceries', 1);
