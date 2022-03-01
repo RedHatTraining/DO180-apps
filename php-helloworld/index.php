27c45283569b8671887a6c1ee03c82745f428c2c
takukouchen <35527622+takukouchen@users.noreply.github.com>
2022-03-01 18:41:52 +0900

    CICD test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "\n\r hello chenzhuoheng S2I \n\r";
+print "\n\r hello chenzhuoheng S2I \n\r";
+print "\n\r hello chenzhuoheng S2I \n\r";
 ?>
