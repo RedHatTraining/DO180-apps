53d0787f32209c931a46e3cf4376707cf86bd005
takukouchen <35527622+takukouchen@users.noreply.github.com>
2022-03-08 16:15:48 +0900

    test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "\n\r hello chenzhuoheng S2I \n\r";
-print "\n\r hello chenzhuoheng S2I \n\r";
-print "\n\r hello chenzhuoheng S2I \n\r";
+print "\n\r But while he's making it impossible to look away from his nation's agony, Zelensky is increasingly running headlong into the war's harsh reality: President Joe Biden and European leaders face political and geopolitical red lines that Russian President Vladimir Putin, a nuclear-armed tyrant, simply does not in his relentless destruction. \n\r";
 ?>
