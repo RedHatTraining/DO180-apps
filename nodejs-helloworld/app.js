bc2258cdd07f5f6e79e4eb263f2e8f548e653cec
wburtz <37351855+wburtz@users.noreply.github.com>
2022-04-26 00:04:19 +0200

    Update app.js
    changes xyz

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! XYZ');
 });
 
 app.listen(8080, function () {
