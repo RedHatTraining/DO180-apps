aaf5aef9152a30e3e76ae10d6074c915ebce10ae
Student User <student@workstation.lab.example.com>
2022-12-12 05:57:47 -0500

    Modified value

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
