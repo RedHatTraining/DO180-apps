5de58b450dc4e1bc9401f7ff6118c7a6f3bb62aa
martin-benko <121024841+martin-benko@users.noreply.github.com>
2023-11-26 15:42:34 +0100

    Update index.php

--- temps/index.php
+++ temps/index.php
@@ -8,7 +8,7 @@
 
 <table>
 <tr>
-    <td>Enter the temperature to convert:</td>
+    <td>Enter the Temperature to convert:</td>
     <td><input type="text" name="temp" id="temp" size="10"></td>
 </tr>
 
